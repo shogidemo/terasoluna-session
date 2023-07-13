f222d87c072341ed35ecfdc96d3e832244d929ed
SATO Sho <shogidemo@gmail.com>
2023-07-13 16:19:22 +0900

    springのコンテキスト設定を行った

--- session-tutorial-init-web/src/main/java/com/example/session/app/cart/CartForm.java
+++ session-tutorial-init-web/src/main/java/com/example/session/app/cart/CartForm.java
@@ -13,6 +13,7 @@ public class CartForm {
         return removedItemsIds;
     }
 
+    @SuppressWarnings("unused") // 未使用警告が表示されるが、フレームワーク経由で呼ばれるので実際は未使用ではない。警告回避のためにアノテーションをつける。
     public void setRemovedItemsIds(Set<String> removedItemsIds) {
         this.removedItemsIds = removedItemsIds;
     }
